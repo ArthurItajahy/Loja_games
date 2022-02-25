286c4d00895690f8d0d25a9e0f37a4dcea1a6e42
emitpool <canalp3rry@gmail.com>
2022-02-25 05:13:39 -0300

    Adionando farmacia

--- lojagames/src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
+++ lojagames/src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
@@ -12,10 +12,9 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
 
 	
-	public List <Produto> findByPrecoGreaterThanOrderByPreco(BigDecimal preco);
-	
+	public List<Produto> findByPrecoGreaterThanOrderByPreco(BigDecimal preco);
 	
 	 
-	public List <Produto> findByPrecoLessThanOrderByPrecoDesc(BigDecimal preco);
+	public List<Produto> findByPrecoLessThanOrderByPrecoDesc(BigDecimal preco);
 	
 }
