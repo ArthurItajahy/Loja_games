6d8abba275bc8f9d70683575cc685659b434d5c9
emitpool <canalp3rry@gmail.com>
2022-03-07 15:35:35 -0300

    Adicionando Novos atalhos no controller e ajustando usuario

--- lojagames/src/main/java/com/generation/lojagames/security/BasicSecurityConfig.java
+++ lojagames/src/main/java/com/generation/lojagames/security/BasicSecurityConfig.java
@@ -37,8 +37,10 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.authorizeRequests().antMatchers("/usuarios/logar").permitAll().antMatchers("/usuarios/cadastrar")
-				.permitAll().antMatchers(HttpMethod.OPTIONS).permitAll().anyRequest().authenticated().and().httpBasic()
+		http.authorizeRequests().antMatchers("/usuarios/logar").permitAll()
+		.antMatchers("/usuarios/cadastrar").permitAll()
+		.antMatchers("/produtos/all").permitAll()
+		.antMatchers(HttpMethod.OPTIONS).permitAll().anyRequest().authenticated().and().httpBasic()
 				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and()
 				.csrf().disable();
 
